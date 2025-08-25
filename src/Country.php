a1bdb02406a61b2ccb2c1d103aa2d37b904df955
Duha Mamdouh <duha.mamdouh@areebgroup.com>
2025-08-25 15:06:20 +0300

    fix: styleci

--- src/Country.php
+++ src/Country.php
@@ -51,13 +51,13 @@ class Country
         // make it as a laravel collection
         $countries = collect($countries);
 
-         // search on the collection using nafath app code
+        // search on the collection using nafath app code
         $country = $countries->where('nafath_app_nationality_code', $code)->first();
 
 		 // get all the attributes and construct the object or throw exception country not found
          if(! $country) {
              throw new Exception('Country not found');
-		}
+		 }
 
 		 return CountryLoader::country($country['iso_3166_1_alpha2'], $hydrate);
     }

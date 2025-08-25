446e23ef566117331d98b2481773db25fa7e2896
Duha Mamdouh <duha.mamdouh@areebgroup.com>
2025-08-25 15:04:35 +0300

    fix: styleci

--- src/Country.php
+++ src/Country.php
@@ -48,8 +48,8 @@ class Country
         // get all countries on the file
         $countries = json_decode(file_get_contents(__DIR__.'/../resources/data/shortlist.json'), true);
 
-		 // make it as a laravel collection
-         $countries = collect($countries);
+        // make it as a laravel collection
+        $countries = collect($countries);
 
 		// search on the collection using nafath app code
         $country = $countries->where('nafath_app_nationality_code', $code)->first();
